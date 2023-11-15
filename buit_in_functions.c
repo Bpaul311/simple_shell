e48a50ee1f899a441411d3ce02a126ab2eb75c99
Bpaul311 <bizimapaul777.com>
2023-11-15 08:44:04 -0800

    added enviromnent

--- buit_in_functions.c
+++ buit_in_functions.c
@@ -26,4 +26,86 @@ int custom_exit(data_of_program *data)
     free_everything(data);
     exit(errno);
 }
+/**
+ * unset_env - Removes the environmnent.
+ * @data: A struct containing the program's data.
+ * Return: 0 on success and another number if it doesnot
+ */
+int unset_env(data_of_program *data)
+{
+	if (data->tokens[1] == NULL)
+		return (0);
+	if (data->tokens[2] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
+	env_remove_key(data->tokens[1], data);
 
+	return (0);
+}
+
+/**
+ * set_env - set the environment.
+ * @data: The struct for the program's data
+ * Return: Zero if success, or another number if specified in the arguments
+ */
+int builtin_set_env(data_of_program *data)
+{
+	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
+		return (0);
+	if (data->tokens[3] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
+
+	set_env(data->tokens[1], data->tokens[2], data);
+
+	return (0);
+}
+/**
+ * Display_env - Displays the environment in which the shell is running.
+ * @data: A structure containing program data.
+ * Return: Zero on success, or a non-zero value if specified in the arguments.
+ */
+int Dsiplay_env(data_of_program *data)
+{
+	int i;
+	char cpname[50] = {'\0'};
+	char *var_cpy = NULL;
+
+	if (!(data->tokens[1]))
+		print_env(data);
+	else
+	{
+		for (i = 0; data->tokens[1][i]; i++)
+		{
+			if (data->tokens[1][i] == '=')
+			{
+				var_cpy = _str_duplicate(fetch_info(cpname, data));
+				if (var_cpy != NULL)
+					env_set_key(cpname, data->tokens[1] + i + 1, data);
+				print_environ(data);
+				if (env_get_key(cpname, data) == NULL)
+				{/* print the variable if it does not exist in the environ */
+					_printf(data->tokens[1]);
+					_printf("\n");
+				}
+				else
+				{/* returns the old value of the var*/
+					set_env(cpname, var_cpy, data);
+					free(var_cpy);
+				}
+				return (0);
+			}
+			cpname[i] = data->tokens[1][i];
+		}
+		errno = 2;
+		perror(data->command_name);
+		errno = 127;
+	}
+	return (0);
+}
--- path.c
+++ path.c
@@ -23,3 +23,95 @@ int find_file(char *full_path)
 	errno = 127;
 	return (127);
 }
+/**
+ * find_path - Tokenize path in the dirs and return it 
+ * @data: A pointer to program's data
+ * Return: Path array of directories
+ */
+char **find_path(data_of_program *data)
+{
+	int i = 0;
+	int num_dirs = 2;
+	char **tokens = NULL;
+	char *PATH;
+
+	/* get the PATH value*/
+	PATH = fetch_info("PATH", data);
+	if ((PATH == NULL) || PATH[0] == '\0')
+	{/*path not found*/
+		return (NULL);
+	}
+
+	PATH = _str_duplicate(PATH);
+
+	/* find the number of dirs in the PATH */
+	for (i = 0; PATH[i]; i++)
+	{
+		if (PATH[i] == ':')
+			num_dirs++;
+	}
+
+	/* reserve space for the array of pointers */
+	tokens = malloc(sizeof(char *) * count_dirs);
+
+	/*tokenize and duplicate each token of path*/
+	i = 0;
+	tokens[i] = _strdup(_strtok(PATH, ":"));
+	while (tokens[i++])
+	{
+		tokens[i] = _strdup(_strtok(NULL, ":"));
+	}
+
+	free(PATH);
+	PATH = NULL;
+	return (tokens);
+
+}
+
+/**
+ * find_program - This finds a program in the path.
+ * @data: A pointer to program's data.
+ * Return: 0 on success, otherwise error code is returned.
+ */
+int find_program(data_of_program *data)
+{
+	int i = 0, ret_code = 0;
+	char **dirs;
+
+	if (!data->command_name)
+		return (2);
+
+	/**if is a full_path or an executable in the same path */
+	if (data->command_name[0] == '/' || data->command_name[0] == '.')
+		return (find_file(data->command_name));
+
+	free(data->tokens[0]);
+	data->tokens[0] = _strcat(_str_duplicate("/"), data->command_name);
+	if (!data->tokens[0])
+		return (2);
+
+	dirs = find_path(data);
+	if (!dirs || !dirs[0])
+	{
+		errno = 127;
+		return (127);
+	}
+	for (i = 0; dirs[i]; i++)
+	{/* appends the function_name to path */
+		dirs[i] = _strcat(dirs[i], data->tokens[0]);
+		ret_code = find_file(dirs[i]);
+		if (ret_code == 0 || ret_code == 126)
+		{/* the file was found, is not a directory and has execute permisions*/
+			errno = 0;
+			free(data->tokens[0]);
+			data->tokens[0] = _str_duplicate(dirs[i]);
+			free_pointers_array(dirs);
+			return (ret_code);
+		}
+	}
+	free(data->tokens[0]);
+	data->tokens[0] = NULL;
+	free_array_of_pointers(dirs);
+	return (ret_code);
+}
+
